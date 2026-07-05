1442d6aaa8d85ab54be5cf7a2820bbf7d226aea2
Erik Kazaryan <erik2007kaz@gmail.com>
2026-07-05 23:02:18 +0700

    adjust global styles and App header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import Simulation from './components/Simulation';
 
-function App() {
-  return <Simulation />;
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center',
+        fontSize: '1.8rem',        
+        margin: '12px 0 6px 0',   
+        color: '#f1f5f9'
+      }}>
+        🧬 Эволюция существ
+      </h1>
+      <Simulation />
+    </div>
+  );
 }
 
-export default App;
